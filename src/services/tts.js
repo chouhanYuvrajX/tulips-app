e787c72bf277995ea278a48dd871c605388c047e
chouhanYuvrajX <chouhanyuvrajsingh819@email.com>
2026-06-25 18:04:45 +0530

    fix: truncate TTS text to 300 chars

--- src/services/tts.js
+++ src/services/tts.js
@@ -4,6 +4,7 @@ const generateSpeech = async (text) => {
   try {
     const tts = new EdgeTTS();
     text = text.replace(/[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F1E0}-\u{1F1FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/gu, '');
+    text = text.substring(0, 300);
     await tts.synthesize(text, 'en-IN-PrabhatNeural', {
       outputFormat: 'audio-24khz-96kbitrate-mono-mp3'
     });
